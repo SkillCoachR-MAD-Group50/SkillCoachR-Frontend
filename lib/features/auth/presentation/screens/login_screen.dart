3807e6543b0b8b23e57f8141210a1d5a56b1a16e
iamlasantha <social.iamlasantha@gmail.com>
2026-03-26 03:48:35 +0530

    fix(auth): add missing firebase_auth import in LoginScreen

--- lib/features/auth/presentation/screens/login_screen.dart
+++ lib/features/auth/presentation/screens/login_screen.dart
@@ -6,6 +6,7 @@ import 'package:go_router/go_router.dart';
 import '../../../../core/theme/app_theme.dart';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import '../../../../core/services/auth_service.dart';
 
 class LoginScreen extends ConsumerStatefulWidget {
